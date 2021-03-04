7b189dfbcc91404245b69d132afa7a71e382cfc5
Alessandro Marega <ale_marega@hotmail.it>
2021-03-04 12:57:26 +0100

    resa più flessibile con linghezza array

--- JSnack3/script.js
+++ JSnack3/script.js
@@ -2,13 +2,14 @@
 var nomi = ["Ale", "Marco", "Luca", "Giovanni", "Mario"];
 var cognomi = ["Marega", "Besozzi", "Kravos", "Malu", "Locovaz"];
 
+// genero variabile lista falsa
 var listaFalsa = [];
 
-
 for (var i = 0; i <= 2; i++) {
-  var j = Math.floor(Math.random()*4);
-  var d = Math.floor(Math.random()*4);
+  var j = Math.floor(Math.random()*nomi.length);
+  var d = Math.floor(Math.random()*cognomi.length);
   listaFalsa[i] = nomi[j] + cognomi[d];
 }
 
+// stampa lista falsa
 console.log(listaFalsa);
