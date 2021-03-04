b7c2e1816109a7b361df5df5bdfe09b34b6063b6
Alessandro Marega <ale_marega@hotmail.it>
2021-03-04 12:58:54 +0100

    aggiunto spazio a listafalsa

--- JSnack3/script.js
+++ JSnack3/script.js
@@ -8,7 +8,7 @@ var listaFalsa = [];
 for (var i = 0; i <= 2; i++) {
   var j = Math.floor(Math.random()*nomi.length);
   var d = Math.floor(Math.random()*cognomi.length);
-  listaFalsa[i] = nomi[j] + cognomi[d];
+  listaFalsa[i] = nomi[j] + " " + cognomi[d];
 }
 
 // stampa lista falsa
